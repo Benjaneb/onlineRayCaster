5dad98187dffbb242be8d7eb5e2394bfee643875
Benjaneb <71073919+Benjaneb@users.noreply.github.com>
2021-02-22 23:21:20 +0100

    mini map, new user pop-up, about page

--- js/main.js
+++ js/main.js
@@ -3,7 +3,24 @@
 let playGame = true;
 
 function toggleControls() {
-    document.querySelector(".controls-background").classList.toggle("hide");
+    document.querySelector(".controls").classList.toggle("hide");
+    if(playGame) {
+        playGame = false;
+    } else {
+        playGame = true;
+    }
+}
+
+function checkUser() {
+    if(JSON.parse(localStorage.getItem("newUser")) === null){
+        document.querySelector(".new-user").classList.toggle("hide");
+        playGame = false;
+        localStorage.setItem("newUser", new Boolean(false));
+    }
+}
+
+function toggleNewUser() {
+    document.querySelector(".new-user").classList.toggle("hide");
     if(playGame) {
         playGame = false;
     } else {
--- js/sketch.js
+++ js/sketch.js
@@ -18,7 +18,7 @@ let map =
 		['&', '.', '.', '.', '.', '.', '#', '.', '.', '.', '.', '.', '#', '#', '.', '.', '.', '#', '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '&'],
 		['&', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '.', '#', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '#', '&'],
 		['&', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '&'],
-		['&', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '0', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '&'],
+		['&', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '&'],
 		['&', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '&'],
 		['&', '#', '.', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '&'],
 		['&', '.', '.', '.', '.', '#', '.', '%', '%', '%', '%', '.', '#', '#', '.', '.', '.', '#', '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '&'],
@@ -38,33 +38,41 @@ let map =
 
 let mapWidth = map[0].length;
 let mapHeight = map.length;
+let tileSize = 12;
 
-let playerX = mapWidth / 2.0;
-let playerY = mapHeight / 2.0;
-let playerAngle = 0.0;
-let fov = 3.14159 / 4.0;
+let player = {
+	x: mapWidth / 2.0,
+	y: mapHeight / 2.0,
+	angle: 0.0
+};
+let fov = 3.14159 / 4;
 let depth = 16;
 
-function setup() {
+function setup() { // Runs once at startup
 	if(canvas.getContext) {
 		createCanvas(window.innerWidth, window.innerHeight);
 	} else {
 		document.querySelector("#noSupport").classList.toggle("hide");
 	}
 	noStroke();
+	//loadData();
+	draw();
+	checkUser();
 }
 
-function draw() {
-	let cosAngle = Math.cos(playerAngle);
-	let sinAngle = Math.sin(playerAngle);
+function draw() { // Loop
+	let cosAngle = Math.cos(player.angle);
+	let sinAngle = Math.sin(player.angle);
 	if(playGame) {
 		background(0);
 		Controls(cosAngle, sinAngle);
 		RayCaster(cosAngle, sinAngle);
+		miniMap();
 	}
+	//saveData();
 }
 
-function windowResized() {
+function windowResized() { // Runs if the window gets resized
   	resizeCanvas(window.innerWidth, window.innerHeight);
 }
 
@@ -73,35 +81,68 @@ function Controls(cosAngle, sinAngle) {
 	let turnSpeed = 2.5 * (deltaTime / 1000);
 	let forwardX = 0;
 	let forwardY = speed;
-	let rotForwardX = cosAngle * forwardX - sinAngle * forwardY;
-	let rotForwardY = sinAngle * forwardX + cosAngle * forwardY;
 	let rightX = speed;
 	let rightY = 0;
+	let rotForwardX = cosAngle * forwardX - sinAngle * forwardY;
+	let rotForwardY = sinAngle * forwardX + cosAngle * forwardY;
+	// let rotBackwardX = -1 * (cosAngle * forwardX - sinAngle * forwardY);
+	// let rotBackwardY = -1 * (sinAngle * forwardX + cosAngle * forwardY);
 	let rotRightX = cosAngle * rightX - sinAngle * rightY;
 	let rotRightY = sinAngle * rightX + cosAngle * rightY;
+	// let rotLeftX = -1 * (cosAngle * rightX - sinAngle * rightY);
+	// let rotLeftY = -1 * (sinAngle * rightX + cosAngle * rightY);
+	let hitBoxRad = 0.4;
+	let colForwardX = cosAngle * hitBoxRad;
+	let colForwardY = sinAngle * hitBoxRad;
+	let colRightX = sinAngle * hitBoxRad;
+	let colRightY = cosAngle * hitBoxRad;
 
+	// Controls
 	if(keyIsDown(87)) {
-		playerY += rotForwardY;
-		playerX += rotForwardX;
+		// if(map[Math.floor(player.y)][Math.floor(player.x + colForwardX)] == '.') {
+        	player.x += rotForwardX;
+    	// }
+    	// if(map[Math.floor(player.y + colForwardY)][Math.floor(player.x)] == '.') {
+        	player.y += rotForwardY;
+    	// }
 	}
 	if(keyIsDown(65)) {
-		playerY -= rotRightY;
-		playerX -= rotRightX;
+		// if(map[Math.floor(player.y)][Math.floor(player.x - colRightX)] == '.') {
+			player.x -= rotRightX;
+    	// }
+    	// if(map[Math.floor(player.y - colRightY)][Math.floor(player.x)] == '.') {
+			player.y -= rotRightY;
+    	// }
 	}
 	if(keyIsDown(83)) {
-		playerY -= rotForwardY;
-		playerX -= rotForwardX;
+		// if(map[Math.floor(player.y)][Math.floor(player.x - colForwardX)] == '.') {
+			player.x -= rotForwardX;
+    	// }
+    	// if(map[Math.floor(player.y - colForwardY)][Math.floor(player.x)] == '.') {
+			player.y -= rotForwardY;
+    	// }
 	}
 	if(keyIsDown(68)) {
-		playerY += rotRightY;
-		playerX += rotRightX;
+		// if(map[Math.floor(player.y)][Math.floor(player.x + colRightX)] == '.') {
+			player.x += rotRightX;
+    	// }
+    	// if(map[Math.floor(player.y + colRightY)][Math.floor(player.x)] == '.') {
+			player.y += rotRightY;
+    	// }
 	}
 	if(keyIsDown(LEFT_ARROW)) {
-		playerAngle += turnSpeed;
+		player.angle += turnSpeed;
 	}
 	if(keyIsDown(RIGHT_ARROW)) {
-		playerAngle -= turnSpeed;
+		player.angle -= turnSpeed;
 	}
+
+	// Collision detection
+	// for(let i = 0; i < 8; i++) {
+	// 	if(map[Math.floor(player.y)][Math.floor(player.x + hitBoxRad)] == '.') {
+	// 		player.x -= hitBoxRad - (Math.floor(player.x + 1) - player.x);
+	// 	}
+	// }
 }
 
 function RayCaster(cosAngle, sinAngle) {
@@ -110,22 +151,25 @@ function RayCaster(cosAngle, sinAngle) {
 	let slutX = punktY;
 	let rayOrdning = 0;
 	let deltaX = slutX - startX;
+	// let rayY = Math.floor(player.y) + 1;
 
 	for(let x = startX; x < slutX; x += deltaX / window.innerWidth) {
 		rayOrdning++;
 		let rayLutning = x / punktY;
 		let y = 0.0;
+		// let rotSlutpunktX = cosAngle * x - sinAngle * punktY + player.x;
+		// let rotSlutpunktY = sinAngle * x + cosAngle * punktY + player.y;
+		// let rayLutning = (rotSlutpunktY - player.y) / (rotSlutpunktX - player.x);
 
 		while(y < punktY) {
 			let rayX = rayLutning * y;
 			let rayY = y;
-			let roteradRayX = cosAngle * rayX - sinAngle * rayY + playerX;
-			let roteradRayY = sinAngle * rayX + cosAngle * rayY + playerY;
+			let roteradRayX = cosAngle * rayX - sinAngle * rayY + player.x;
+			let roteradRayY = sinAngle * rayX + cosAngle * rayY + player.y;
 			y += 0.02;
 
 			if(roteradRayX >= 0 && roteradRayX < mapWidth && roteradRayY >= 0 && roteradRayY < mapHeight) {
 				if(map[Math.floor(roteradRayY)][Math.floor(roteradRayX)] != '.') {
-					//console.log("pip");
 					let avståndPlayerVägg = y;
 					let kollumnLängd = window.innerHeight / avståndPlayerVägg;
 					let kollumnStartY = window.innerHeight / 2 - kollumnLängd / 2;
@@ -143,15 +187,51 @@ function RayCaster(cosAngle, sinAngle) {
 						case '%': c = [130, 130, 130]; break;
 						case '0': c = [72, 212, 205]; break;
 					}
-					fill(c[0] / (avståndPlayerVägg * 0.5 + 1), c[1] / (avståndPlayerVägg * 0.5 + 1), c[2] / (avståndPlayerVägg * 0.5 + 1));
-					/*for(let i = kollumnStartY; i < kollumnSlutY; i++) {
-						point(rayOrdning, i);
-					}*/
+					c[0] /= (avståndPlayerVägg * 0.5 + 1);
+					c[1] /= (avståndPlayerVägg * 0.5 + 1);
+					c[2] /= (avståndPlayerVägg * 0.5 + 1);
+					fill(c[0], c[1], c[2]);
 					rect(rayOrdning, kollumnStartY, 1, kollumnLängd);
+					miniMapRays(roteradRayX, roteradRayY);
 					break;
 				}
 			}
 		}
 	}
-	//console.log("frame, " + playerX + " " + playerY + " " + deltaTime);
+	// console.log("frame, " + player.x + " " + player.y + " " + Math.round(1 / (deltaTime / 1000)));
+}
+
+function miniMapRays(roteradRayX, roteradRayY) {
+	stroke(150, 0, 0);
+	strokeWeight(1);
+	line(player.x * tileSize, player.y * tileSize, roteradRayX * tileSize, roteradRayY * tileSize);
+	noStroke();
+}
+
+function miniMap() {
+	noFill();
+	strokeWeight(1);
+	stroke(50, 50, 150);
+	for(let i = 0; i < mapHeight; i++) {
+		for(let j = 0; j < mapWidth; j++) {
+			if(map[i][j] != '.')
+				fill(100, 100, 200);
+			rect(j * tileSize, i * tileSize, tileSize, tileSize);
+			noFill();
+		}
+	}
+	stroke(200, 0, 0);
+	strokeWeight(10);
+	point(player.x * tileSize, player.y * tileSize);
+	noStroke();
+}
+
+function saveData() {
+	window.addEventListener("beforeunload", () => {
+		localStorage.setItem("player", JSON.stringify(player));
+	});
+}
+
+function loadData() {
+	player = JSON.parse(localStorage.getItem("player"));
 }
